362dd694e98b7689dc9646810dda1d1b2a1b3d61
Giulio <giulio_denardi@intuit.com>
2019-10-23 06:47:55 -0300

    feat: adding health check

---
+++ src/main/java/br/com/itau/postservice/controller/HealthCheck.java
@@ -0,0 +1,14 @@
+package br.com.itau.postservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void healthCheck() {}
+}
