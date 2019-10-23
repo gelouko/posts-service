e4009c96195826ed92cfabcd39394518da4d2f86
Giulio <giulio_denardi@intuit.com>
2019-10-23 06:11:17 -0300

    feat: adding controller for resources

--- src/main/java/br/com/itau/postservice/controller/CommentsController.java
+++ src/main/java/br/com/itau/postservice/controller/CommentsController.java
@@ -1,11 +1,21 @@
 package br.com.itau.postservice.controller;
 
+import static br.com.itau.postservice.exceptions.PostServiceExceptionType.DATABASE_ALREADY_POPULATED;
+
+import br.com.itau.postservice.exceptions.PostServiceException;
 import br.com.itau.postservice.jsonplaceholder.JsonPlaceholderClient;
-import br.com.itau.postservice.jsonplaceholder.payload.Comment;
-import br.com.itau.postservice.model.SaveCommentsExecution;
-import br.com.itau.postservice.repository.SaveCommentsExecutionRepository;
+import br.com.itau.postservice.jsonplaceholder.payload.CommentPayload;
+import br.com.itau.postservice.model.Comment;
+import br.com.itau.postservice.model.Post;
+import br.com.itau.postservice.repository.CommentsRepository;
+import br.com.itau.postservice.repository.PostRepository;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,25 +26,45 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommentsController {
 
     private JsonPlaceholderClient jsonPlaceholderClient;
-    private SaveCommentsExecutionRepository saveCommentsExecutionRepository;
+    private PostRepository postRepository;
+    private CommentsRepository commentsRepository;
 
     @Autowired
     public CommentsController(JsonPlaceholderClient jsonPlaceholderClient,
-            SaveCommentsExecutionRepository saveCommentsExecutionRepository) {
+            PostRepository postRepository,
+            CommentsRepository commentsRepository) {
         this.jsonPlaceholderClient = jsonPlaceholderClient;
-        this.saveCommentsExecutionRepository = saveCommentsExecutionRepository;
+        this.postRepository = postRepository;
+        this.commentsRepository = commentsRepository;
     }
 
     @GetMapping
-    public List<Comment> list() {
-        return jsonPlaceholderClient.fetchComments();
+    public Page<Comment> list(Pageable pageable) {
+        return commentsRepository.findAll(pageable);
     }
 
     @PostMapping
-    public SaveCommentsExecution save() {
-        SaveCommentsExecution saveCommentsExecution = new SaveCommentsExecution();
+    public void save() {
+        if (postRepository.count() > 0) {
+            throw new PostServiceException(DATABASE_ALREADY_POPULATED);
+        }
 
-        return saveCommentsExecutionRepository.save(saveCommentsExecution);
+        List<CommentPayload> commentPayloads = jsonPlaceholderClient.fetchComments();
+
+        Map<Long, List<CommentPayload>> postComments = commentPayloads.stream()
+                .collect(Collectors.groupingBy(CommentPayload::getPostId));
+
+        List<Post> posts = postRepository
+                .saveAll(jsonPlaceholderClient.fetchPosts(postComments.keySet()).stream()
+                        .map(Post::new).collect(Collectors.toSet()));
+
+        Map<Long, Post> postsMap = posts.stream()
+                .collect(Collectors.toMap(Post::getId, post -> post));
+
+        Set<Comment> comments = commentPayloads.stream().map(comment -> new Comment(comment,
+                postsMap.get(comment.getPostId()))).collect(Collectors.toSet());
+
+        commentsRepository.saveAll(comments);
     }
 
 }
--- src/main/java/br/com/itau/postservice/controller/PostsController.java
+++ src/main/java/br/com/itau/postservice/controller/PostsController.java
@@ -1,27 +1,37 @@
 package br.com.itau.postservice.controller;
 
 import br.com.itau.postservice.jsonplaceholder.JsonPlaceholderClient;
-import br.com.itau.postservice.jsonplaceholder.payload.Post;
+import br.com.itau.postservice.model.Post;
+import br.com.itau.postservice.repository.PostRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("posts")
 public class PostsController {
 
-    private JsonPlaceholderClient jsonPlaceholderClient;
+    private PostRepository postRepository;
 
     @Autowired
-    public PostsController(JsonPlaceholderClient jsonPlaceholderClient) {
-        this.jsonPlaceholderClient = jsonPlaceholderClient;
+    public PostsController(PostRepository postRepository) {
+        this.postRepository = postRepository;
     }
 
     @GetMapping
-    public List<Post> list(@RequestParam List<Long> id) {
-        return jsonPlaceholderClient.fetchPosts(id);
+    public Page<Post> list(Pageable pageable) {
+        return postRepository.findAll(pageable);
     }
+
+    @GetMapping("top")
+    public List<Post> listTop() {
+        PageRequest pageRequest = PageRequest.of(0, 5);
+        return postRepository.findTopPosts(pageRequest);
+    }
+
 }
